5afc3b795db24f272e0e92975b2cb7445591e926
VrundaBalgi <68437782+VrundaBalgi@users.noreply.github.com>
2020-07-17 22:11:52 +0530

    Update dow_header.h

--- dow_header.h
+++ dow_header.h
@@ -10,6 +10,6 @@ void find_Year_Century(int year,int *yy,int *cc);
 
 void find_month_cent_codes(int month,int cc,int *mcode,int *cent_code);
 
-void find_dayofweek(int date,int mcode,int yy,int cent_code);
+void find_dayofweek(int date,int mcode,int yy,int cent_code,int leap);
 
 #endif // DOW_HEADER_H_INCLUDED
